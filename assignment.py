be80f65d413ebe0cf4425120624768a786c47cac
angiekamii <angelajoan138@gmail.com>
2023-03-21 08:47:01 +0300

    intial commit

---
+++ assignment.py
@@ -0,0 +1,15 @@
+
+
+number = int(input("enter marks:"))
+if number >=80:
+    print("A")
+else:
+    if number >=60<80:
+        print("B")
+
+    else:
+        if number >=50<60:
+            print("C")
+        else:
+            if number <50:
+                print("D")
---
+++ calculator.py
@@ -0,0 +1,17 @@
+num1 = float(input("enter first number:"))
+num2 = float(input('enter second number:'))
+
+print("enter 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 for  division")
+
+choice = int(input("enter choice 1-4:"))
+
+if choice == 1:
+    print(num1 + num2)
+elif choice == 2:
+    print(num1 - num2)
+elif choice == 3:
+    print(num1 * num2)
+elif choice == 4:
+    print(num1/num2)
+
+
